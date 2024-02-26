c30400f7b96b5867cc51829f8deee6d64be924ad
Kostjusz97 <kostasan9797@gmail.com>
2024-02-26 11:19:15 +0100

    postres

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
 });
 
 module.exports = sequelize;
